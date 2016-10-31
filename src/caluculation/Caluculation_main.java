fc2a3cc12ca67873cf914f97cff9522c56f7a457
50516047 <ramishi.br1v4h41rt.46841@ezweb.ne.jp>
2016-10-31 15:51:29 +0900

    20161024 submit

---
+++ src/caluculation/Caluculation_main.java
@@ -0,0 +1,16 @@
+package caluculation;
+import java.util.Scanner;
+
+public class Caluculation_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input Number m > ");
+		int m = Integer.parseInt(scan.next());
+		
+		System.out.print("Input Number n > ");
+		int n = Integer.parseInt(scan.next());
+	}
+
+}
